49a8ad63f99e9bb0b816449e225f8a186ae48299
Tom Carroll <tomalancarroll@gmail.com>
2014-04-11 14:53:03 -0400

    Added rescue helicopter and drone models; Added scripts to rotate helicopter blades

---
+++ Assets/Scripts/DroneBladeController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DroneBladeController : MonoBehaviour {
+	private float rotationsPerMinute =3000f;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(0,6 * rotationsPerMinute * Time.deltaTime,0);
+	}
+}
---
+++ Assets/Scripts/RotorController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotorController : MonoBehaviour {
+	private float rotationsPerMinute =180f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(0,0,6 * rotationsPerMinute * Time.deltaTime);
+	}
+}
