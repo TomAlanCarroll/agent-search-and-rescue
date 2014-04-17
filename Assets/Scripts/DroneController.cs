cb11058655a63bcbd59682c85a0ada479b4ea151
Tom Carroll <tomalancarroll@gmail.com>
2014-04-16 22:29:15 -0400

    Added drone controller and spawning of soldiers and drones

---
+++ Assets/Scripts/DroneController.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DroneController : MonoBehaviour {
+	public enum Strategy {
+		SPREAD_OUT
+	}
+
+	// Constants
+	public const float TURN_RATE = 200f;
+	public const float FLYING_SPEED = 4.47f; // 10mph is approx 4.47 m/s
+
+	public Strategy strategy;
+
+		
+	void Start () {
+		strategy = Strategy.SPREAD_OUT;
+	}
+	
+	void Update () {	
+		// Look for friendly soldiers
+
+
+//		Travel (strategy);
+	}
+
+	private void Travel()
+	{
+//		float speed = 0f;
+//		Vector3 moveDirection;
+//
+//		// Only apply gravity if we are not moving
+//		if (!isMoving)
+//		{
+//			// Only apply gravity if we are falling
+//			moveDirection = Vector3.zero;
+//			
+//			// Apply gravity
+//			moveDirection.y -= GRAVITY * Time.deltaTime;
+//			
+//			// Move towards the position
+//			controller.Move(moveDirection);
+//			return;
+//		}
+//
+//		if (state == State.RUNNING)
+//		{
+//			speed = RUNNING_SPEED;
+//		}
+//		else if (state == State.WALKING)
+//		{
+//			speed = WALKING_SPEED;
+//		}		
+//		
+//		// Rotate towards destination
+//		Vector3 rotation = Vector3.RotateTowards(transform.forward, (transform.position - destination), 
+//		                                         Mathf.Deg2Rad * TURN_RATE * Time.deltaTime, 1);
+//		transform.rotation = Quaternion.LookRotation(rotation);
+//		
+//		moveDirection = speed * Vector3.Normalize(transform.position - destination) * Time.deltaTime;
+//		
+//		// Apply gravity
+//		moveDirection.y -= GRAVITY * Time.deltaTime;
+//		
+//		// Move towards the position
+//		controller.Move(moveDirection);
+	}
+}
--- Assets/Scripts/SpawnController.cs
+++ Assets/Scripts/SpawnController.cs
@@ -2,12 +2,16 @@
 
 public class SpawnController : MonoBehaviour {
 	public GameObject friendlySoldierPrefab;
+	public GameObject dronePrefab;
+
+	// Constants
+	public const int NUM_DRONES = 10;
 
 
 	// Use this for initialization
 	void Start () {
 
-		// Initializer friendly soldiers in the buildings
+		// Initialize friendly soldiers in the buildings
 		foreach (GameObject building in GameObject.FindGameObjectsWithTag("BuildingSkeleton"))
 		{
 			int numSoldiers = Random.Range (10, 15);
@@ -15,16 +19,19 @@ public class SpawnController : MonoBehaviour {
 			{
 				Vector3 spawnPosition = new Vector3(
 					Random.Range (building.renderer.bounds.min.x, building.renderer.bounds.max.x),
-					Random.Range (building.renderer.bounds.min.y, building.renderer.bounds.max.y),
+					Random.Range (building.renderer.bounds.min.y, 20),
 					Random.Range (building.renderer.bounds.min.z, building.renderer.bounds.max.z));
 
 				Instantiate (friendlySoldierPrefab, spawnPosition, Quaternion.identity);
 			}
 		}
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+		// Initialize search drones
+		Vector3 helicopterPosition = GameObject.FindGameObjectWithTag ("Helicopter").transform.position;
+
+		for (int i = 0; i < NUM_DRONES; i++)
+		{			
+			Instantiate (dronePrefab, helicopterPosition, Quaternion.identity);
+		}
 	}
 }
