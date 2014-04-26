3d5a5e00a05b7affdeda448aeb328481b3f5cbdf
Tom Carroll <tomalancarroll@gmail.com>
2014-04-26 11:25:13 -0400

    Added 10 spawn points to the scene for soldier groups between 8-12 soldiers

--- Assets/Scripts/SpawnController.cs
+++ Assets/Scripts/SpawnController.cs
@@ -3,41 +3,47 @@
 public class SpawnController : MonoBehaviour {
 	public GameObject friendlySoldierPrefab;
 	public GameObject dronePrefab;
-
+	
 	// Constants
 	public const int NUM_DRONES = 10;
-	public const int MIN_SOLDIERS_PER_BUILDING = 8;
-	public const int MAX_SOLDIERS_PER_BUILDING = 12;
-
-
+	public const int MIN_SOLDIERS_PER_SQUAD = 8;
+	public const int MAX_SOLDIERS_PER_SQUAD = 12;
+	public const float MAX_SQUAD_RADIUS = 10f;
+	
+	
 	// Use this for initialization
 	void Start () {
-
+		
 		// Initialize friendly soldiers in the buildings
-		foreach (GameObject building in GameObject.FindGameObjectsWithTag("BuildingSkeleton"))
+		foreach (GameObject spawnPoint in GameObject.FindGameObjectsWithTag("SpawnPoint"))
 		{
-			int numSoldiers = Random.Range (10, 15);
-			for (int i = 0; i < numSoldiers; i++)
-			{
-				Vector3 spawnPosition = new Vector3(
-					Random.Range (building.renderer.bounds.min.x, building.renderer.bounds.max.x),
-					20,
-					Random.Range (building.renderer.bounds.min.z, building.renderer.bounds.max.z));
+			int numSoldiers = Random.Range (MIN_SOLDIERS_PER_SQUAD, MAX_SOLDIERS_PER_SQUAD);
+			float deltaDegrees = 360f / numSoldiers;
 
-				Instantiate (friendlySoldierPrefab, spawnPosition, Quaternion.identity);
+			Vector3 platoonCentroid = spawnPoint.transform.position;
+			
+			for (float degree = 0f; degree < 360f; degree += deltaDegrees)
+			{
+				float radius = Random.Range(1, MAX_SQUAD_RADIUS);
+				Vector3 spawnPosition = new Vector3(
+					platoonCentroid.x + (radius * Mathf.Sin(degree * Mathf.Deg2Rad)),
+					platoonCentroid.y,
+					platoonCentroid.z + (radius * Mathf.Cos(degree * Mathf.Deg2Rad)));
+				
+				Instantiate (friendlySoldierPrefab, spawnPosition, Quaternion.Euler(0f, Random.Range(0f, 360f), 0f));
 			}
 		}
-
+		
 		// Initialize search drones
 		Vector3 helicopterPosition = GameObject.FindGameObjectWithTag ("Helicopter").transform.position;
-
+		
 		for (int i = 0; i < NUM_DRONES; i++)
 		{		
 			// Show the first drone camera in the corner of the screen
 			if (i == 0)
 			{
 				var drone = Instantiate (dronePrefab, helicopterPosition, Quaternion.identity);
-
+				
 				((GameObject)drone).camera.depth = 1;
 			}
 			else
